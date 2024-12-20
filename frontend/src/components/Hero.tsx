4f603f5267e8053bd5ebc41727949a055da04370
Muhammad Dzaky Arya Saputra Budianto <113959018+JustDz@users.noreply.github.com>
2024-12-20 07:53:18 +0700

    Commit update

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -5,41 +5,47 @@ import { Reveal } from "../animate/Reveal";
 
 const Hero = () => {
   return (
-    <section className="bg-first-bg relative" id="Hero">
-      <div className="max-w-7xl mx-auto">
-        <div className="flex flex-col md:flex-row items-center lg:h-[90vh] justify-between">
-          {/*--------------- Home ---------------*/}
-          <div className="md:w-[70%] mb-8 md:mb-0 flex flex-col space-y-4 px-6 lg:mt-0 mt-10">
-            <Reveal>
-              <p className="text-font-clr font-medium md:text-2xl text-xl mb-4 font-poppins">
-                IOT X Machine Learning
-              </p>
-            </Reveal>
+    <section id="Hero">
+      <div className="bg-first-bg relative h-[100vh]" >
+        <div className="absolute inset-0 opacity-15">
+          <div
+            className="w-full h-full"
+            style={{
+              backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`,
+              backgroundRepeat: "repeat",
+              mixBlendMode: "overlay",
+            }}
+          />
+        </div>
 
-            <Reveal>
-              <h1 className="font-extrabold text-font-clr tracking-normal text-7xl lg:leading-snug font-poppins">
-                Sistem Pendeteksi Postur Tubuh
-              </h1>
-            </Reveal>
+        <div className="max-w-7xl mx-auto">
+          <div className="text-center flex flex-col md:flex-row items-center lg:h-[90vh] justify-between">
+            {/*--------------- Home ---------------*/}
+            <div className="w-full md:mb-0 flex flex-col space-y-4 px-6 lg:mt-0 mt-10">
+              <Reveal>
+                <p className="mt-16 font-main text-heading-clr font-medium md:text-2xl text-xl mb-4">
+                  IOT X Machine Learning
+                </p>
+              </Reveal>
 
-            <Reveal>
-              <p className="text-font-clr font-medium md:text-lg text-base font-poppins">
-                Dengan sistem ini, Anda dapat memantau dan memperbaiki posisi
-                duduk Anda untuk menjaga kesehatan tubuh.
-              </p>
-            </Reveal>
+              <Reveal>
+                <h1 className="font-extrabold text-heading-clr tracking-wider text-9xl lg:leading-snug font-heading">
+                  Sistem Pendeteksi <br />
+                  Postur Tubuh
+                </h1>
+              </Reveal>
 
-            {/*--------------- Button Left ---------------*/}
-            <Reveal>
-              <div className="flex justify-start gap-x-6 dark:text-white">
-                <Link
-                  to="/stream"
-                  className="mt-5 group flex items-center justify-center disabled:opacity-50 disabled:hover:opacity-50 hover:opacity-95 ring-none rounded-lg shadow-lg font-semibold py-3 px-6 font-dm text-sm sm:text-base dark:bg-gray-700 dark:border-gray-700 dark:border-b-gray-900 transform transition-transform duration-300 ease-in-out hover:scale-100"
-                >
-                  Periksa Sekarang
-                </Link>
-              </div>
-            </Reveal>
+              {/*--------------- Button Left ---------------*/}
+              <Reveal>
+                <div className="items-center flex justify-center gap-x-6 dark:text-white h-[11vh]">
+                  <Link
+                    to="/stream"
+                    className="my-8 group flex items-center justify-center disabled:opacity-50 disabled:hover:opacity-50 hover:opacity-95 ring-none rounded-full shadow-lg font-semibold py-3 px-6 font-dm text-sm sm:text-base bg-btn-clr text-body-clr font-heading tracking-widest transform transition-all duration-300 ease-in-out hover:scale-110 hover:rotate-[-3deg] hover:bg-second-bg">
+                    Periksa Sekarang
+                  </Link>
+                </div>
+              </Reveal>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/components/Monitoring.tsx
+++ frontend/src/components/Monitoring.tsx
@@ -4,143 +4,150 @@ import { Reveal } from "../animate/Reveal";
 
 const DashboardMonitoring: React.FC = () => {
   return (
-    <div className="bg-gray-900 text-white min-h-screen mt-">
-      <div className="max-w-7xl mx-auto px-6 lg:px-8">
-        {/* Header */}
-        <Reveal>
-          <header className="p-6">
-            <h1 className="text-3xl font-bold mt-6 text-center">
-              Monitoring Postur Tubuh
-            </h1>
-          </header>
-        </Reveal>
-
-        {/* Main Content */}
-        <main className="pt-6 pb-6">
-          {/* Dashboard Monitoring */}
+    <section id="Monitoring">
+      <div className="bg-body-clr text-white min-h-220">
+        <div className="max-w-7xl mx-auto px-6 lg:px-8">
+          {/* Header */}
           <Reveal>
-            <section className=" bg-gray-800 p-6 rounded-lg shadow-lg mb-8">
-              <Reveal>
-                <h2 className="text-2xl font-semibold mb-4">
-                  Dashboard Monitoring Postur
-                </h2>
-              </Reveal>
-
-              {/* Status Postur */}
-              <div className="mb-6">
-                <Reveal>
-                  <h3 className="text-xl font-medium">
-                    Status Postur Saat Ini
-                  </h3>
-                </Reveal>
-
-                <Reveal>
-                  <p className="text-lg mt-2">
-                    Postur Anda saat ini:
-                    <span className="font-bold text-green-500">Baik</span>
-                  </p>
-                </Reveal>
-
-                <Reveal>
-                  <p className="text-sm mt-1">Durasi: 2 Jam 30 Menit</p>
-                </Reveal>
-              </div>
-
-              {/* Waktu Postur */}
-              <div className="mb-6">
-                <Reveal>
-                  <h3 className="text-xl font-medium">Waktu Postur</h3>
-                </Reveal>
-
-                <Reveal>
-                  <p className="text-sm mt-2">
-                    Postur baik telah dipertahankan selama 2 Jam 30 Menit.
-                  </p>
-                </Reveal>
-              </div>
-
-              {/* Pengingat */}
-              <div className="mb-6">
-                <Reveal>
-                  <h3 className="text-xl font-medium">Pengingat</h3>
-                </Reveal>
-
-                <Reveal>
-                  <p className="text-sm mt-2">
-                    Jangan lupa untuk sesekali memperbaiki posisi duduk Anda
-                    agar tetap sehat!
-                  </p>
-                </Reveal>
-              </div>
-            </section>
+            <header className="p-6 mb-5 mt-12" id="Monitoring">
+              <h1 className="font-text tracking-widest text-6xl font-bold mt-6 text-center">
+                Monitoring Postur Tubuh
+              </h1>
+            </header>
           </Reveal>
 
-          {/* Riwayat Postur */}
-          <Reveal>
-            <section className="bg-gray-800 p-6 rounded-lg shadow-lg mb-8">
+          {/* Main Content */}
+          <main className="pt-6 pb-6">
+            {/* Dashboard Monitoring and Riwayat Postur */}
+            <div className="font-text grid grid-cols-1 lg:grid-cols-2 gap-8 mb-8">
+              {/* Dashboard Monitoring */}
               <Reveal>
-                <h2 className="text-2xl font-semibold mb-4">Riwayat Postur</h2>
+                <section className="h-[50vh] bg-gray-800 p-6 rounded-lg shadow-lg">
+                  <Reveal>
+                    <h2 className="text-center text-2xl font-semibold mb-8">
+                      Dashboard Monitoring Postur
+                    </h2>
+                  </Reveal>
+
+                  {/* Status Postur */}
+                  <div className="mb-8">
+                    <Reveal>
+                      <h3 className="text-xl font-medium">
+                        Status Postur Saat Ini
+                      </h3>
+                    </Reveal>
+
+                    <Reveal>
+                      <p className="text-lg mt-2">
+                        Postur Anda saat ini:
+                        <span className="font-bold text-green-500"> Baik</span>
+                      </p>
+                    </Reveal>
+
+                    <Reveal>
+                      <p className="text-sm mt-1">Durasi: 2 Jam 30 Menit</p>
+                    </Reveal>
+                  </div>
+
+                  {/* Waktu Postur */}
+                  <div className="mb-8">
+                    <Reveal>
+                      <h3 className="text-xl font-medium">Waktu Postur</h3>
+                    </Reveal>
+
+                    <Reveal>
+                      <p className="text-sm mt-2">
+                        Postur baik telah dipertahankan selama 2 Jam 30 Menit.
+                      </p>
+                    </Reveal>
+                  </div>
+
+                  {/* Pengingat */}
+                  <div className="mb-6">
+                    <Reveal>
+                      <h3 className="text-xl font-medium">Pengingat</h3>
+                    </Reveal>
+
+                    <Reveal>
+                      <p className="text-sm mt-2">
+                        Jangan lupa untuk sesekali memperbaiki posisi duduk Anda
+                        agar tetap sehat!
+                      </p>
+                    </Reveal>
+                  </div>
+                </section>
               </Reveal>
 
-              {/* Table Riwayat */}
-              <div className="overflow-x-auto">
+              {/* Riwayat Postur */}
+              <Reveal>
+                <section className="h-[50vh] bg-gray-800 p-6 rounded-lg shadow-lg">
+                  <Reveal>
+                    <h2 className="text-center text-2xl font-semibold mb-4">
+                      Riwayat Postur
+                    </h2>
+                  </Reveal>
+
+                  {/* Table Riwayat */}
+                  <div className="overflow-x-auto">
+                    <Reveal>
+                      <table className="min-w-full table-auto">
+                        <thead>
+                          <tr className="bg-gray-700">
+                            <th className="px-4 py-2 text-left">Tanggal</th>
+                            <th className="px-4 py-2 text-left">
+                              Status Postur
+                            </th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr className="border-b border-gray-600">
+                            <td className="px-4 py-2">2024-12-05 08:00</td>
+                            <td className="px-4 py-2">Buruk</td>
+                          </tr>
+                          <tr className="border-b border-gray-600">
+                            <td className="px-4 py-2">2024-12-04 14:00</td>
+                            <td className="px-4 py-2">Baik</td>
+                          </tr>
+                          <tr className="border-b border-gray-600">
+                            <td className="px-4 py-2">2024-12-03 10:30</td>
+                            <td className="px-4 py-2">Buruk</td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </Reveal>
+                  </div>
+                </section>
+              </Reveal>
+            </div>
+
+            {/* Grafik Postur */}
+            <Reveal>
+              <section className="font-text bg-gray-800 p-6 rounded-lg shadow-lg">
                 <Reveal>
-                  <table className="min-w-full table-auto">
-                    <thead>
-                      <tr className="bg-gray-700">
-                        <th className="px-4 py-2 text-left">Tanggal</th>
-                        <th className="px-4 py-2 text-left">Status Postur</th>
-                        <th className="px-4 py-2 text-left">Durasi</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr className="border-b border-gray-600">
-                        <td className="px-4 py-2">2024-12-05 08:00</td>
-                        <td className="px-4 py-2">Buruk</td>
-                        <td className="px-4 py-2">30 Menit</td>
-                      </tr>
-                      <tr className="border-b border-gray-600">
-                        <td className="px-4 py-2">2024-12-04 14:00</td>
-                        <td className="px-4 py-2">Baik</td>
-                        <td className="px-4 py-2">1 Jam 15 Menit</td>
-                      </tr>
-                      <tr className="border-b border-gray-600">
-                        <td className="px-4 py-2">2024-12-03 10:30</td>
-                        <td className="px-4 py-2">Buruk</td>
-                        <td className="px-4 py-2">45 Menit</td>
-                      </tr>
-                    </tbody>
-                  </table>
+                  <h2 className="text-center text-2xl font-semibold mb-4">
+                    Grafik Postur
+                  </h2>
                 </Reveal>
-              </div>
-            </section>
-          </Reveal>
 
-          {/* Grafik Postur */}
-          <Reveal>
-            <section className="bg-gray-800 p-6 rounded-lg shadow-lg">
-              <Reveal>
-                <h2 className="text-2xl font-semibold mb-4">Grafik Postur</h2>
-              </Reveal>
+                {/* Dummy Grafik */}
+                <Reveal>
+                  <div className="bg-gray-700 w-full h-64 flex justify-center items-center text-xl font-bold text-gray-400">
+                    [Grafik Postur Tubuh]
+                  </div>
+                </Reveal>
 
-              {/* Dummy Grafik */}
-              <Reveal>
-                <div className="bg-gray-700 w-full h-64 flex justify-center items-center text-xl font-bold text-gray-400">
-                  [Grafik Postur Tubuh]
-                </div>
-              </Reveal>
-
-              <Reveal>
-                <p className="text-sm mt-2 text-center">
-                  Visualisasi grafik postur tubuh berdasarkan data yang
-                  tercatat.
-                </p>
-              </Reveal>
-            </section>
-          </Reveal>
-        </main>
+                <Reveal>
+                  <p className="text-sm mt-2 text-center">
+                    Visualisasi grafik postur tubuh berdasarkan data yang
+                    tercatat.
+                  </p>
+                </Reveal>
+              </section>
+            </Reveal>
+          </main>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
